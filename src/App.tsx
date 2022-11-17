99b68ac7cfaba5ea2b449a03b0aa2f9384b75bdf
Vadym Olefirenko <vadim@MacBook-Air-Vadym.local>
2022-11-17 21:42:33 +0200

    Add return annotation to the App function

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AppHeader from "./components/AppHeader/AppHeader";
 import Board from "./components/Board/Board";
 import {arrayOfNumbers, shuffle} from "./utils/utils";
 
-function App() {
+function App(): JSX.Element {
   const [cards, setCards] = useState<Array<number>>([]);
 
   useEffect(() => setCards(shuffle(arrayOfNumbers)), []);
